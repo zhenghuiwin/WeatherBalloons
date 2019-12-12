af8043c4fbdac7a1c98e42bda48047dec0d07a88
Zhw <bluegene.hao@gmail.com>
2019-12-13 00:39:10 +0800

    Add conf

---
+++ Sources/WeatherBalloons/config/Config.swift
@@ -0,0 +1,29 @@
+//
+//  Config.swift
+//  RGF_Monitor
+//
+//  Created by zhenghuiwin on 2018/10/14.
+//
+
+import Foundation
+
+public class Config {
+    
+    private static let configPath = "\(FileManager.default.currentDirectoryPath)/conf/BalloonsConf.json"
+    
+    public class func load() throws -> ConfigInfo {
+        let configData = try Data(contentsOf: URL(fileURLWithPath: configPath))
+
+        let jsonDecoder = JSONDecoder()
+        let configInfo = try jsonDecoder.decode(ConfigInfo.self, from: configData)
+        
+        return configInfo
+    }
+    
+    public class func save(configInfo: ConfigInfo) throws {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(configInfo)
+        
+        try data.write(to: URL(fileURLWithPath: configPath))
+    }
+}
---
+++ Sources/WeatherBalloons/config/ConfigInfo.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  RGF_Monitor
+//
+//  Created by zhenghuiwin on 2018/10/14.
+//
+
+import Foundation
+
+public struct ConfigInfo: Codable {
+    public let host: String
+    public let user: String
+    public let pawd: String
+}
+
